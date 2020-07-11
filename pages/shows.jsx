4ca6116aee30685d421380a72b70731fa0ae7667
Brian Cilenti <briancilenti@gmail.com>
2020-07-11 15:55:26 -0400

    remove stellas shows

--- pages/shows.jsx
+++ pages/shows.jsx
@@ -13,23 +13,9 @@ const Shows = (props) => {
                 <hr />
 
                 <div className="row start-xs my-2 txt-silver">
-                    <h5 className="">August 1st &mdash; Stella's Music Club, Cleveland</h5>
-                </div>
-                <hr />
-
-                <div className="row start-xs my-2 txt-silver">
-                    <h5 className="">August 29th &mdash; Stella's Music Club, Cleveland</h5>
-                </div>
-                <hr />
-
-                <div className="row start-xs my-2 txt-silver">
                     <h5 className="">September 5th &mdash; Shooters on the Water, Cleveland</h5>
                 </div>
-                <hr />
-
-                <div className="row start-xs my-2 txt-silver">
-                    <h5 className="">September 26th &mdash; Stella's Music Club, Cleveland</h5>
-                </div>
+                {/* <hr /> */}
             </div>
         </>
     );
