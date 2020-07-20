fd4a0fc1c617e9d739d37868dae906861f2e4647
keepforever <briancilenti@gmail.com>
2020-07-20 19:54:21 -0400

    update shows

--- pages/shows.jsx
+++ pages/shows.jsx
@@ -3,19 +3,17 @@ const Shows = (props) => {
         <>
             <div className="masthead masthead-shows"></div>
             <div className="container txt-silver">
-                <div className="row start-xs my-2 txt-silver">
-                    <h1>Summer 2020</h1>
-                </div>
-
-                <div className="row start-xs my-2 txt-silver">
-                    <h5 className="">July 25th &mdash; Shooters on the Water, Cleveland</h5>
+                <div className="row start-xs mt-2 mb-4 txt-silver bottom-xs">
+                    <h1 className="m">Summer 2🦠2🦠</h1>
+                    <p className="mx-3 txt-italic">
+                        {' '}
+                        <small>(many COVID-19 cancellations later...)</small>{' '}
+                    </p>
                 </div>
                 <hr />
-
-                <div className="row start-xs my-2 txt-silver">
+                <div className="row start-xs mt-5 txt-silver">
                     <h5 className="">September 5th &mdash; Shooters on the Water, Cleveland</h5>
                 </div>
-                {/* <hr /> */}
             </div>
         </>
     );
