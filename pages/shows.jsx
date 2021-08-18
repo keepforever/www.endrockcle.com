e9e69b757cd6bf08bcddb114beebf456e86ccf62
Brian Cilenti <briancilenti@gmail.com>
2021-08-17 22:48:58 -0400

    update shows

--- pages/shows.jsx
+++ pages/shows.jsx
@@ -9,15 +9,52 @@ const Shows = (props) => {
                 <hr />
 
                 <div className="row start-xs mt-5 txt-silver">
-                    <h4 className="">June 19th &mdash; Shooters on the Water, Cleveland: 9p - 1a</h4>
-                </div>
-                <div className="row start-xs mt-5 txt-silver">
-                    <h4 className="">August 27th &mdash; Collision Bend Brewery &mdash; Cleveland: 9p - 12a </h4>
+                    <h4 className="">
+                        August 27th &mdash;{' '}
+                        <a
+                            className="txt-larger show-location"
+                            rel="download noopener noreferrer"
+                            target="_blank"
+                            href="https://www.collisionbendbrewery.com/"
+                        >
+                            Collision Bend Brewery
+                        </a>{' '}
+                        &mdash; Cleveland &mdash; 8:00p - 11:00p{' '}
+                    </h4>
                 </div>
                 <div className="row start-xs mt-5 txt-silver">
                     <h4 className="">
-                        September 4th &mdash; McArthur's Brew House &mdash; 2721 Front St, Cuyahoga Falls, OH 44221:
-                        6:30p - 10:30p{' '}
+                        September 4th &mdash;{' '}
+                        <a
+                            className="txt-larger show-location"
+                            rel="download noopener noreferrer"
+                            target="_blank"
+                            href="https://mcarthursbrewhouse.com/"
+                        >
+                            {' '}
+                            McArthur's Brew House{' '}
+                        </a>{' '}
+                        &mdash; 2721 Front St, Cuyahoga Falls, OH 44221 &mdash; 6:30p - 10:30p{' '}
+                    </h4>
+                </div>
+
+                <div className="row start-xs mt-2 mb-4 txt-silver bottom-xs">
+                    <h1 className="m">Winter 2022</h1>
+                </div>
+                <hr />
+
+                <div className="row start-xs mt-5 txt-silver">
+                    <h4 className="">
+                        January 15th &mdash;{' '}
+                        <a
+                            className="txt-larger show-location"
+                            rel="download noopener noreferrer"
+                            target="_blank"
+                            href="https://www.frontst.social/"
+                        >
+                            Front St. Social
+                        </a>{' '}
+                        &mdash; Berea &mdash; 8:00p - 11:00p{' '}
                     </h4>
                 </div>
             </div>
