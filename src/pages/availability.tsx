dc2753330e10f62983665d172c55207a031076f3
Brian Cilenti <briancilenti@Brians-MacBook-Pro-2.local>
2021-12-27 23:07:17 -0500

    feat: add copy date

--- src/pages/availability.tsx
+++ src/pages/availability.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @next/next/no-img-element */
 import Link from 'next/link';
+import { useState } from 'react';
 
 import AppShell from '@/components/layouts/AppShell';
 import Seo from '@/components/Seo';
@@ -14,6 +15,16 @@ import {
 } from '@/utils/blackout';
 
 const Shows: React.FC = () => {
+  const [searchValue, setSearchValue] = useState<string>('');
+  const [showCopyIndex, setShowCopyIndex] = useState<string>('');
+
+  const handleCopyToClipboard = (date: string, index: string) => {
+    navigator.clipboard.writeText(date);
+    setShowCopyIndex(index);
+    setTimeout(() => {
+      setShowCopyIndex('');
+    }, 2000);
+  };
   return (
     <AppShell>
       <Seo templateTitle='Availability' />
@@ -34,45 +45,102 @@ const Shows: React.FC = () => {
         </div>
       </div>
 
-      <div className='py-12'>
-        <div className='mx-auto max-w-4xl lg:px-8 px-4'>
-          <div className='mb-24'>
-            <div className='mt-6'>
-              <h2 className='text-2xl lg:text-4xl font-extrabold tracking-tight text-gray-300'>
-                Choose From Below
-              </h2>
-              <p className='mt-4 text-xl text-gray-300'>
-                <strong>The Dates Below</strong> are an approximation of our
-                current availability throughout 2022. <br /> <br /> If you don't
-                see a date there you would like to book, please ask anyways.
-              </p>
-            </div>
+      <div className='mx-auto max-w-4xl lg:px-8 px-4'>
+        <div className=''>
+          <div className='mt-6'>
+            <h2 className='text-2xl lg:text-4xl font-extrabold tracking-tight text-gray-300'>
+              Choose From Below
+            </h2>
+            <p className='mt-4 text-xl text-gray-300'>
+              <strong>The dates below</strong> are an approximation of our
+              current availability throughout 2022. We're primarily looking for
+              Fri, Sat, Sun gigs, but willing to play weekdays for select
+              events.
+            </p>
+
+            <p className='mt-4 mb-6 text-xl text-gray-300'>
+              <strong>Use the search box</strong> below to filter the available
+              dates by day of the week (e.g. Monday, Tuesday, etc.) or by month
+              (e.g. Jan, Feb, etc).
+            </p>
           </div>
+        </div>
+      </div>
+
+      {/* Search Bar */}
+
+      <div className='mx-auto max-w-4xl lg:px-8 px-4 text-gray-300 mb-12'>
+        <label htmlFor='email' className='block text-xl font-medium'>
+          Filter Dates
+        </label>
+        <div className='mt-3'>
+          <input
+            className='shadow-sm bg-gray-500 w-full md:w-3/4 text-2xl rounded-md inline-block mr-3 mb-3 placeholder-gray-200 placeholder-opacity-60'
+            type='search'
+            name='search'
+            id='search'
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder='Mon, Tue, Feb, Jan, Oct, etc.'
+          />
+          <button
+            onClick={() => setSearchValue('')}
+            type='button'
+            className='inline-flex items-center px-6 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+          >
+            Clear Search
+          </button>
         </div>
       </div>
 
       <div className='mx-auto max-w-4xl lg:px-8 px-4'>
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
-          {availableDates.map((date) => (
-            <div
-              key={convertDateToString(date)}
-              className='flex-col bg-gray-600 text-gray-300 py-4 px-4 rounded-lg backdrop-filter backdrop-blur-lg bg-opacity-60 hover:shadow-lg transition duration-500 ease-in-out hover:drop-shadow-lg hover:bg-opacity-70'
-            >
-              <div className='font-extrabold text-2xl mb-2'>
-                {getMonthName(date)}, {getOrdinalNum(date.getDate())}
-              </div>
+          {availableDates
+            .filter((date) => {
+              const monthName = getMonthName(date);
+              const dayName = getDayName(date);
+              return (
+                monthName.toLowerCase().includes(searchValue.toLowerCase()) ||
+                dayName.toLowerCase().includes(searchValue.toLowerCase())
+              );
+            })
+            .map((date, index) => (
+              <div
+                key={convertDateToString(date)}
+                className='flex bg-gray-600 text-gray-300 py-4 px-4 rounded-lg backdrop-filter backdrop-blur-lg bg-opacity-60 hover:shadow-lg transition duration-500 ease-in-out hover:drop-shadow-lg hover:bg-opacity-70'
+              >
+                <div style={{ flex: 3 }} className='flex-col w/70'>
+                  <div className='font-extrabold text-2xl mb-2'>
+                    {getMonthName(date)}, {getOrdinalNum(date.getDate())}
+                  </div>
 
-              <div className='font-extrabold text-2xl mb-2'>
-                {getDayName(date)}
+                  <div className='font-extrabold text-2xl mb-2'>
+                    {getDayName(date)}
+                  </div>
+                  <div className='font-normal'>{date.toDateString()}</div>
+                </div>
+                <div
+                  style={{ flex: 1 }}
+                  className='flex justify-center items-center w/30'
+                >
+                  <button
+                    onClick={() => {
+                      handleCopyToClipboard(
+                        `${getDayName(date)} ${getMonthName(
+                          date
+                        )}, ${getOrdinalNum(
+                          date.getDate()
+                        )}\n${convertDateToString(date)}`,
+                        String(index)
+                      );
+                    }}
+                    className='px-2 py-1 text-gray-300 text-md  border-2 border-gray-300 hover:border-erc-yellow hover:text-erc-yellow transition duration-500 ease-in-out'
+                  >
+                    {String(index) === showCopyIndex ? 'Copied!' : 'Copy'}
+                  </button>
+                </div>
               </div>
-              <div className='font-normal'>{date.toDateString()}</div>
-              {/* <Link href='/contact'>
-                <a className='px-2 py-1 text-gray-300 text-xl  border-2 border-gray-300 hover:border-erc-yellow hover:text-erc-yellow transition duration-500 ease-in-out'>
-                  Book Now
-                </a>
-              </Link> */}
-            </div>
-          ))}
+            ))}
         </div>
       </div>
 
