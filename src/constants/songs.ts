5da6f31ab529c6088913daaf06e55cb80a112204
Brian Cilenti <briancilenti@gmail.com>
2024-03-03 12:37:10 -0500

    song ids

--- src/constants/songs.ts
+++ src/constants/songs.ts
@@ -1,345 +1,280 @@
 export const songs = [
   {
-    id: 0,
     artist: 'Bowling for Soup',
     title: '1985',
   },
   {
-    id: 1,
     artist: 'Pearl Jam',
     title: 'Alive',
   },
   {
-    id: 2,
     artist: 'Smash Mouth',
     title: 'All Star',
   },
   {
-    id: 3,
     artist: 'Blink-182',
     title: 'All the Small Things',
   },
   {
-    id: 4,
     artist: 'Tom Petty and the Heartbreakers',
     title: 'American Girl',
   },
   {
-    id: 5,
     artist: 'Green Day',
     title: 'Basket Case',
   },
   {
-    id: 6,
     artist: 'Weezer',
     title: 'Buddy Holly',
   },
   {
-    id: 7,
     artist: 'Rage Against the Machine',
     title: 'Bulls on Parade',
   },
   {
-    id: 8,
     artist: 'Hole',
     title: 'Celebrity Skin',
   },
   {
-    id: 9,
     artist: 'The Presidents of the United States of America',
     title: 'Cleveland Rocks',
   },
   {
-    id: 10,
     artist: 'Semisonic',
     title: 'Closing Time',
   },
   {
-    id: 11,
     artist: 'The Offspring',
     title: 'Come Out and Play',
   },
   {
-    id: 12,
     artist: 'Radiohead',
     title: 'Creep',
   },
   {
-    id: 13,
     artist: 'Blink-182',
     title: 'Dammit',
   },
   {
-    id: 14,
     artist: '311',
     title: 'Down',
   },
   {
-    id: 15,
     artist: 'Rob Zombie',
     title: 'Dragula',
   },
   {
-    id: 16,
     artist: 'Foo Fighters',
     title: 'Everlong',
   },
   {
-    id: 17,
     artist: 'Sum 41',
     title: 'Fat Lip',
   },
   {
-    id: 18,
     artist: 'Harvey Danger',
     title: 'Flagpole Sitta',
   },
   {
-    id: 19,
     artist: 'Bush',
     title: 'Glycerine',
   },
   {
-    id: 20,
     artist: 'Better Than Ezra',
     title: 'Good',
   },
   {
-    id: 21,
     artist: 'Green Day',
     title: 'Good Riddance (Time of Your Life)',
   },
   {
-    id: 22,
     artist: 'Gin Blossoms',
     title: 'Hey Jealousy',
   },
   {
-    id: 23,
     artist: 'Misfits',
     title: 'Hybrid Moments',
   },
   {
-    id: 24,
     artist: 'The Darkness',
     title: 'I Believe in a Thing Called Love',
   },
   {
-    id: 25,
     artist: 'Ramones',
     title: 'I Wanna Be Sedated',
   },
   {
-    id: 26,
     artist: 'Eve 6',
     title: 'Inside Out',
   },
   {
-    id: 27,
     artist: 'Stone Temple Pilots',
     title: 'Interstate Love Song',
   },
   {
-    id: 28,
     artist: 'Rage Against the Machine',
     title: 'Killing in the Name',
   },
   {
-    id: 29,
     artist: 'The Strokes',
     title: 'Last Nite',
   },
   {
-    id: 30,
     artist: 'Nirvana',
     title: 'Lithium',
   },
   {
-    id: 31,
     artist: 'Green Day',
     title: 'Longview',
   },
   {
-    id: 32,
     artist: 'Bush',
     title: 'Machinehead',
   },
   {
-    id: 33,
     artist: 'Alice in Chains',
     title: 'Man in the Box',
   },
   {
-    id: 34,
     artist: 'The Killers',
     title: 'Mr. Brightside',
   },
   {
-    id: 35,
     artist: 'Foo Fighters',
     title: 'My Hero',
   },
   {
-    id: 36,
     artist: 'Weezer',
     title: 'My Name Is Jonas',
   },
   {
-    id: 37,
     artist: 'Lit',
     title: 'My Own Worst Enemy',
   },
   {
-    id: 38,
     artist: 'Stone Temple Pilots',
     title: 'Plush',
   },
   {
-    id: 39,
     artist: 'Rancid',
     title: 'Ruby Soho',
   },
   {
-    id: 40,
     artist: 'Beastie Boys',
     title: 'Sabotage',
   },
   {
-    id: 41,
     artist: 'Sublime',
     title: 'Santeria',
   },
   {
-    id: 42,
     artist: 'Weezer',
     title: "Say It Ain't So",
   },
   {
-    id: 43,
     artist: 'Third Eye Blind',
     title: 'Semi-Charmed Life',
   },
   {
-    id: 44,
     artist: 'The White Stripes',
     title: 'Seven Nation Army',
   },
   {
-    id: 45,
     artist: 'Marcy Playground',
     title: 'Sex and Candy',
   },
   {
-    id: 46,
     artist: 'Collective Soul',
     title: 'Shine',
   },
   {
-    id: 47,
     artist: 'Cake',
     title: 'Short Skirt / Long Jacket',
   },
   {
-    id: 48,
     artist: 'Goo Goo Dolls',
     title: 'Slide',
   },
   {
-    id: 49,
     artist: 'Nirvana',
     title: 'Smells Like Teen Spirit',
   },
   {
-    id: 50,
     artist: 'Blur',
     title: 'Song 2',
   },
   {
-    id: 51,
     artist: 'Fountains of Wayne',
     title: "Stacy's Mom",
   },
   {
-    id: 52,
     artist: 'Fall Out Boy',
     title: "Sugar, We're Going Down",
   },
   {
-    id: 53,
     artist: 'Bryan Adams',
     title: "Summer of '69",
   },
   {
-    id: 54,
     artist: 'Wheatus',
     title: 'Teenage Dirtbag',
   },
   {
-    id: 55,
     artist: 'Good Charlotte',
     title: 'The Anthem',
   },
   {
-    id: 56,
     artist: 'Jimmy Eat World',
     title: 'The Middle',
   },
   {
-    id: 57,
     artist: 'The Smashing Pumpkins',
     title: 'Today',
   },
   {
-    id: 58,
     artist: 'Chumbawamba',
     title: 'Tubthumping',
   },
   {
-    id: 59,
     artist: 'Red Hot Chili Peppers',
     title: 'Under the Bridge',
   },
   {
-    id: 60,
     artist: 'Kings of Leon',
     title: 'Use Somebody',
   },
   {
-    id: 61,
     artist: 'Sublime',
     title: 'What I Got',
   },
   {
-    id: 62,
     artist: 'Blink-182',
     title: "What's My Age Again?",
   },
   {
-    id: 63,
     artist: 'Green Day',
     title: 'When I Come Around',
   },
   {
-    id: 64,
     artist: 'The Killers',
     title: 'When You Were Young',
   },
   {
-    id: 65,
     artist: 'Pixies',
     title: 'Where is My Mind?',
   },
   {
-    id: 66,
     artist: 'Oasis',
     title: 'Wonderwall',
   },
   {
-    id: 67,
     artist: 'The Cranberries',
     title: 'Zombie',
   },
-];
+].map((song, index) => ({
+  ...song,
+  id: index + 1,
+}));
 
 // a function that takes in the song list above and returns a list sorted by title alphabetically and give a new sequential id
 const sortSongs = (
